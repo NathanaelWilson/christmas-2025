01412e11af4c11637e6c6299283d19fd3ea986a8
NathanaelWilson <nathanaelwilsonangelo@gmail.com>
2025-12-05 00:18:27 +0700

    Remove .next folder from repo

--- .next/dev/server/app/api/register/route.js
+++
@@ -1,9 +0,0 @@
-var R=require("../../../chunks/[turbopack]_runtime.js")("server/app/api/register/route.js")
-R.c("server/chunks/node_modules_next_e13b0bc0._.js")
-R.c("server/chunks/node_modules_@supabase_storage-js_dist_module_829f4f3c._.js")
-R.c("server/chunks/node_modules_@supabase_auth-js_dist_module_02e1b12e._.js")
-R.c("server/chunks/node_modules_fccfbb27._.js")
-R.c("server/chunks/[root-of-the-server]__f07d6d8d._.js")
-R.c("server/chunks/_next-internal_server_app_api_register_route_actions_688c24b5.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/register/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/app-route.js { INNER_APP_ROUTE => \"[project]/app/api/register/route.ts [app-route] (ecmascript)\" } [app-route] (ecmascript)").exports
--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"8LmvN99EaY/HDYw5Kujyje7aWuLdckox7NUZnmvNcbk=\"\n}"
